e4e9ce0463bdd7116c8922df936d46809abd8d59
Dr. Raed <drraed.jb@gmail.com>
2023-07-11 03:30:21 +0200

    listicon

---
+++ lib/listicon.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+List<Icon> listicon=[
+const Icon(Icons.star,color: Colors.white,),
+const Icon(Icons.table_restaurant,color: Colors.brown,),
+const Icon(Icons.chair,color: Colors.red,),
+const Icon(Icons.egg,color: Colors.white,),
+const Icon(Icons.sunny,color: Colors.orange,),
+const Icon(Icons.train,color: Colors.red,),
+
+];
