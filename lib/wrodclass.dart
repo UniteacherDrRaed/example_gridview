21cecde4bc337fc6cf74c7177f24e6809f48eb66
Dr. Raed <drraed.jb@gmail.com>
2023-07-11 03:42:29 +0200

    edit wordclass

--- lib/wrodclass.dart
+++ lib/wrodclass.dart
@@ -1,11 +1,11 @@
 class WordClass{
-  String? _english_name;
-  String? _german_name;
-  String? _arabic_name;
+  final String _english_name;
+  final String _german_name;
+  final String _arabic_name;
   WordClass(this._english_name,this._german_name,this._arabic_name);
   @override
   String toString()
   {
-    return ${_english_name}, ${_german_name},${_arabic_name};
+    return "${_english_name}, ${_german_name},${_arabic_name}";
   }
 }
